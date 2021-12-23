8dc50a411cd86899429f41eed3ccda66e8d818ce
Kevin Kantarama <42340855+kevinkant@users.noreply.github.com>
2021-12-23 20:09:21 +0100

    Update ChatBot.jsx

--- farmafollow/src/pages/Chatbot/ChatBot.jsx
+++ farmafollow/src/pages/Chatbot/ChatBot.jsx
@@ -4,120 +4,149 @@ import { ThemeProvider } from "styled-components";
 
 //Logica voor de bot
 const logica = [
-
   //Welkombericht
   {
     id: "hello-world",
     message: "Hallo! Waar kan ik je mee helpen?",
-    trigger: 'ingreep',
+    trigger: "ingreep",
   },
 
   //Kiezen van de ingreep
   {
-    id: 'ingreep',
+    id: "ingreep",
     options: [
-      { value: 1, label: "Keelamandelen", trigger: "KeelInfo"},
-      { value: 2, label: "Neusamandelen", trigger: "NeusInfo"},
-      { value: 3, label: "Trommelvliesbuisjes", trigger: "TrommelInfo"},
+      { value: 1, label: "Keelamandelen", trigger: "KeelInfo" },
+      { value: 2, label: "Neusamandelen", trigger: "NeusInfo" },
+      { value: 3, label: "Trommelvliesbuisjes", trigger: "TrommelInfo" },
     ],
   },
   //Info bericht, weergeeft vervolgens de opties gegeven in de overeenmkomende trigger
   {
     id: "KeelInfo",
-    message: 'Wat wil je hierover weten?',
-    trigger: 'Keel',
+    message: "Wat wil je hierover weten?",
+    trigger: "Keel",
   },
   {
     id: "NeusInfo",
-    message: 'Wat wil je hierover weten?',
-    trigger: 'Neus',
+    message: "Wat wil je hierover weten?",
+    trigger: "Neus",
   },
   {
     id: "TrommelInfo",
-    message: 'Wat wil je hierover weten?',
-    trigger: 'Trommel',
+    message: "Wat wil je hierover weten?",
+    trigger: "Trommel",
   },
 
-
-  //Keel ingreep info 
+  //Keel ingreep info
   {
-    id: 'Keel',
+    id: "Keel",
     options: [
-      { value: 1, label: "Tips", trigger: "KeelTips"},
-      { value: 2, label: "Medicatie", trigger: "KeelMeds"},
-      { value: 3, label: "Mijn kind heeft pijn", trigger: "KeelPijn"},
+      { value: 1, label: "Tips", trigger: "KeelTips" },
+      { value: 2, label: "Medicatie", trigger: "KeelMeds" },
+      { value: 3, label: "Mijn kind heeft pijn", trigger: "KeelPijn" },
     ],
   },
   {
-    id: 'KeelTips',
-    message: 'Hier zijn een aantal handige tips',
+    id: "KeelTips",
+    component: (
+      <div>
+        <ul>
+          <li>Koude drankjes en waterijsjes hebben een pijnstillend effect en vermindert de kans op nabloeding</li>
+          <li>Drink regelamtig water en eet licht verteerbaar voedsel.</li>
+          <li>Het is aangeraden de eerste 2 weken niet te gaan zwemmen (chloorwater kan het slijmvlies irriteren)</li>
+        </ul>
+      </div>
+    ),
+    trigger: 'MeerInfoKeel'
+  },
+  //Vragen of er nog info nodig is
+  {
+    id: 'MeerInfoKeel',
+    message: "Wil je meer informatie over de keelamandelen?",
+    trigger: 'Keuze'
   },
   {
-    id: 'KeelMeds',
-    message:'test'
+    id:'Keuze',
+    options: [
+      { value: 1, label: "Ja", trigger: "Keel" },
+      { value: 2, label: "Nee", trigger: "Einde" },
+    ]
+  },
+  // {
+  //   id:'NogIets',
+  //   message: 'Kan ik nog ergens mee helpen?',
+  //   trigger: 'AndereOpties'
+  // },
+  // //
+  // {
+  //   id:'AndereOpties',
+  //   options: [
+  //     { value: 1, label: "Ja", trigger: "ingreep" },
+  //     { value: 2, label: "Nee", trigger: "Einde" },
+  //   ]
+  // },
+  {
+    id:'Einde',
+    message: 'Veel beterschap!'
   },
   {
-    id: 'KeelPijn',
-    message:'test'
+    id: "KeelMeds",
+    message: "test",
+  },
+  {
+    id: "KeelPijn",
+    message: "test",
   },
 
-  //Neus ingreep info 
+  //Neus ingreep info
   {
-    id: 'Neus',
+    id: "Neus",
     options: [
-      { value: 1, label: "Tips", trigger: "NeusTips"},
-      { value: 2, label: "Medicatie", trigger: "NeusMeds"},
-      { value: 3, label: "Mijn kind heeft pijn", trigger: "NeusPijn"},
+      { value: 1, label: "Tips", trigger: "NeusTips" },
+      { value: 2, label: "Medicatie", trigger: "NeusMeds" },
+      { value: 3, label: "Mijn kind heeft pijn", trigger: "NeusPijn" },
     ],
   },
   {
-    id: 'NeusTips',
-    message:'test'
-
+    id: "NeusTips",
+    message: "test",
   },
   {
-    id: 'NeusMeds',
-    message:'test'
+    id: "NeusMeds",
+    message: "test",
   },
   {
-    id: 'NeusPijn',
-    message:'test'
+    id: "NeusPijn",
+    message: "test",
   },
-
 
   //Trommel ingreep info
   {
-    id: 'Trommel',
+    id: "Trommel",
     options: [
-      { value: 1, label: "Tips", trigger: "TrommelTips"},
-      { value: 2, label: "Medicatie", trigger: "TrommelMeds"},
-      { value: 3, label: "Mijn kind heeft pijn", trigger: "TrommelPijn"},
+      { value: 1, label: "Tips", trigger: "TrommelTips" },
+      { value: 2, label: "Medicatie", trigger: "TrommelMeds" },
+      { value: 3, label: "Mijn kind heeft pijn", trigger: "TrommelPijn" },
     ],
   },
   {
-    id: 'TrommelTips',
-    message:'test'
-
+    id: "TrommelTips",
+    message: "test",
   },
   {
-    id: 'TrommelMeds',
-    message:'test'
+    id: "TrommelMeds",
+    message: "test",
   },
   {
-    id: 'TrommelPijn',
-    message:'test'
+    id: "TrommelPijn",
+    message: "test",
   },
-
-
-
 ];
 
 const Chatbot = () => {
   return (
     <div>
-      <ChatBot 
-      steps={logica}
-      botDelay={500} />
+      <ChatBot steps={logica} botDelay={500} userDelay={200} floating={true} />
     </div>
   );
 };
