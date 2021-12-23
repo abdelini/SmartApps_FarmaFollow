0979301ddb991f88b5737184235e61e0ea485528
Kevin Kantarama <42340855+kevinkant@users.noreply.github.com>
2021-12-23 21:15:44 +0100

    Chatbot popup vertonen op pagina

--- SmartApps_FarmaFollow/src/App.js
+++ SmartApps_FarmaFollow/src/App.js
@@ -7,6 +7,8 @@ import Home from './pages/Home'
 import Child from './pages/Child'
 import Profile from './pages/Profile'
 import BottomNavbar from './components/BottomNavbar';
+import Logboek from './pages/Logboek/LogboekScherm';
+import Chatbot from './pages/Chatbot/ChatBot';
 
 function App() {
   return (
@@ -17,9 +19,12 @@ function App() {
           <Route path='/home' component={Home}/>
           <Route path='/profile' component={Profile}/>
           <Route path='/child' component={Child}/>
+          <Route path='/logboek' component={Logboek}/>
+          <Route path='/chatbot' component={Chatbot}/>
         </Routes>
         <Card/>
         <BottomNavbar/>
+        <Chatbot />
       </Router>
     </>
   );
